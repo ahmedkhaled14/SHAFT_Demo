3068ce282b7d70706f6985f55d63139529b6d49b
Ahmed Khaled <68038931+ahmedkhaled14@users.noreply.github.com>
2023-08-16 10:03:56 +0300

    update yml

--- src/test/java/tests/UserRegistrationTest.java
+++ src/test/java/tests/UserRegistrationTest.java
@@ -2,7 +2,6 @@ package tests;
 
 import com.shaft.driver.SHAFT;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.*;
